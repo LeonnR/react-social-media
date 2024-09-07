5c04db8b68e85e8282b406cba8c80425e695087d
leonreuben25 <101970574+LeonnR@users.noreply.github.com>
2024-09-07 20:38:39 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Profile from "./pages/profile/Profile"
+import Register from "./pages/register/Register"
+import Favicon from "react-favicon";
+import { useState } from "react";
+
+
+
+function App() {
+  return <Home/>
+}
+
+export default App;
+  
